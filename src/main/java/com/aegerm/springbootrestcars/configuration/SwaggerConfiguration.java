bedfeafa39bfa8c18561a3d56ce1de869d10f181
Alexandre Eger Marques <alexandre.marques@cwi.com.br>
2020-09-06 14:01:22 -0300

    Implementado documentação de API com Swagger.

---
+++ src/main/java/com/aegerm/springbootrestcars/configuration/SwaggerConfiguration.java
@@ -0,0 +1,38 @@
+package com.aegerm.springbootrestcars.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .contact(new Contact("Alexandre Eger Marques", "", ""))
+                .title("Cars")
+                .description("Documentação API de Carros")
+                .license("Apache Licence Version 2.0")
+                .licenseUrl("https://apache.org")
+                .version("1.0")
+                .build();
+    }
+}
